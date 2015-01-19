cebb164e8e504209c9a9b6a1b713d2137344b84f
veggieboy <meanomenestcaesar@gmail.com>
2015-01-19 11:10:29 -0700

    Update CardList.java

--- src/CardList.java
+++ src/CardList.java
@@ -1,1 +1,32 @@
 // create class CardList
+import java.util.*;
+public class CardList
+{
+  private ArrayList<Card> cards;
+  private Random random=new Random();
+  public CardList()
+  {
+    cards=new ArrayList<Card>();
+  }
+  public int getSize()
+  {
+    return cards.size();
+  }
+  public void addCardToBottom(Card card)
+  {
+    cards.add(card);
+  }
+  public void addCardToTop(Card card)
+  {
+    cards.add(0,card);
+  }
+  public void takeCardFromTop()
+  {
+    cards.remove(0);
+  }
+  public void removeRandomCard()
+  {
+    int randCardNum=random.nextInt(cards.size());
+    cards.remove(cards.get(randCardNum));
+  }
+}
